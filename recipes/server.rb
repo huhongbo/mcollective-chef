3872d4f7928e2726b3c9f75f6027cd2e9843c2ac
dn365 <duning3650@gmail.com>
2012-07-17 14:23:33 +0800

    修改chefnode.txt 生成内容

--- recipes/server.rb
+++ recipes/server.rb
@@ -67,7 +67,7 @@ ruby_block "store node data locally" do
     node.run_list.roles.each do |role|
         state.puts("role.#{role}")
     end
-    node.tag.each do |tag|
+    noderun_list.tags.each do |tag|
       state.puts("tag.#{tag}")
     end    
 
