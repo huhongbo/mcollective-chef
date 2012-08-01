bcdba918cd6f867d30ca8d657c354d512c6b0eaf
dn365 <duning3650@gmail.com>
2012-08-01 11:16:04 +0800

    add collective client gem and recipe

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "dn365@163.com"
 license          "All rights reserved"
 description      "Installs/Configures mcollective-chef mcollective 0.2.1"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 
 %w[ ubuntu hpux aix centos redhat ].each do |os|
   supports os
--- recipes/client.rb
+++ recipes/client.rb
@@ -4,3 +4,15 @@
 template "/etc/mcollective/client.cfg" do
   source "client.cgf.erb"
 end
+
+remote_directory "/var/chef/gems" do
+  source "gems"
+  recursive true
+end
+
+gem_package "mcollective-client" do
+  source "/var/chef/gems/mcollective-client-2.1.1.gem"
+  options "--no-ri --no-rdoc"
+  version "2.1.1"
+  action :install
+end
